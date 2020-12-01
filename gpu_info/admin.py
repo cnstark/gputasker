6b159c69e1f825e29e7f34ce2f49c8b1793e15ea
Yuhao Wang <yuhaow97@gmail.com>
2020-12-02 01:05:11 +0800

    feature: display gpu info in server info page

--- gpu_info/admin.py
+++ gpu_info/admin.py
@@ -2,12 +2,38 @@ from django.contrib import admin
 from .models import GPUServer, GPUInfo
 
 
+class GPUInfoInline(admin.TabularInline):
+    model = GPUInfo
+    fields = ('index', 'name', 'utilization', 'memory_usage', 'server', 'free', 'complete_free', 'update_at')
+    readonly_fields = ('index', 'name', 'utilization', 'memory_usage', 'server', 'free', 'complete_free', 'update_at')
+
+    def memory_usage(self, obj):
+        memory_total = obj.memory_total
+        memory_used = obj.memory_used
+        return '{:d} / {:d} MB ({:.0f}%)'.format(memory_used, memory_total, memory_used / memory_total * 100)
+    
+    memory_usage.short_description = '显存占用率'
+
+    def get_extra(self, request, obj, **kwargs):
+        return 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj):
+        return False
+
+
 @admin.register(GPUServer)
 class GPUServerAdmin(admin.ModelAdmin):
     list_display = ('ip', 'hostname', 'valid', 'can_use')
     list_editable = ('can_use',)
     search_fields = ('ip', 'hostname', 'valid', 'can_use')
     list_display_links = ('ip',)
+    inlines = (GPUInfoInline,)
     ordering = ('ip',)
     readonly_fields = ('hostname',)
 
